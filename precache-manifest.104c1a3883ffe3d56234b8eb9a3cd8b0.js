e431fdab3794d17481e0ccb03484605143a03683
Lokesh Manchanda <lokesh.8055.m@gmail.com>
2020-08-29 12:26:04 +0530

    Updates

---
+++ precache-manifest.104c1a3883ffe3d56234b8eb9a3cd8b0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cda34032df972d3540a024ef13c97b",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "c55f892aa3d6158abd45",
+    "url": "/react-test/static/css/main.5a6baeca.chunk.css"
+  },
+  {
+    "revision": "14a158f0a7818608653d",
+    "url": "/react-test/static/js/2.0f1ee747.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/react-test/static/js/2.0f1ee747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c55f892aa3d6158abd45",
+    "url": "/react-test/static/js/main.426a8e81.chunk.js"
+  },
+  {
+    "revision": "ac81ef67f3e4c5bbf2f4",
+    "url": "/react-test/static/js/runtime-main.33001144.js"
+  },
+  {
+    "revision": "2b3807ed6f3cc17a6bc8b4d275b3e2dd",
+    "url": "/react-test/static/media/DINPro-Black.2b3807ed.ttf"
+  },
+  {
+    "revision": "0de2428e3dee4e94fb83c020cdc02ff2",
+    "url": "/react-test/static/media/DINPro-Bold.0de2428e.ttf"
+  },
+  {
+    "revision": "85e6672392ab81e4165e64926dd66dce",
+    "url": "/react-test/static/media/DINPro-Light.85e66723.ttf"
+  },
+  {
+    "revision": "0888425abd5aaed8ae7401daa0395566",
+    "url": "/react-test/static/media/DINPro-Medium.0888425a.ttf"
+  },
+  {
+    "revision": "86fda98796bb4100ba905535a77723bd",
+    "url": "/react-test/static/media/DINPro-Regular.86fda987.ttf"
+  },
+  {
+    "revision": "619ee97eae1c5e93f5d68a9157b2466c",
+    "url": "/react-test/static/media/college_02.619ee97e.jpg"
+  }
+]);
